3cda4fe1edc733a4df5027876e17ebb58998cd95
mclaughlinjr <153246892+mclaughlinjr@users.noreply.github.com>
2024-11-26 17:44:39 -0700

    Added Page.js with under construction message

---
+++ page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Welcome</h1>
+      <p>Page Under Construction</p>
+    </div>
+  );
+}
+
+export default App;
